5803fc4db4d85876744673aec319d26e81397e42
Trabajo <trabajo@ramiros-macbook-pro.local>
2019-02-11 08:11:24 -0600

    Initial Commit

---
+++ ScrabbleTests/ScrabbleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScrabbleTests.swift
+//  ScrabbleTests
+//
+//  Created by Trabajo on 2/11/19.
+//  Copyright © 2019 Trabajo. All rights reserved.
+//
+
+import XCTest
+@testable import Scrabble
+
+class ScrabbleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
